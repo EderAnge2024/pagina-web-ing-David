f5c885111004108d52c9756b027fe31b6792503b
ClonNetflix <ederangeloingabauitsta@gmail.com>
2025-05-01 19:54:44 -0500

    agregados el store

--- frontend/src/store/ClienteStore.js
+++ frontend/src/store/ClienteStore.js
@@ -5,7 +5,7 @@ const useClienteStore = create((set)=>({
     clientes: [],
     addCliente: async(cliente)=>{
         try {
-            const response = await axios.post('http://localhost:3001/cliente',cliente)
+            const response = await axios.post('http://localhost:3001/clientes',cliente)
             set((state)=>({clientes: [...state.clientes, response.data]}))// crea una copia el "..."
         } catch (error) {
             console.log("Error adding cliente", error.message)
@@ -13,7 +13,7 @@ const useClienteStore = create((set)=>({
     },
     fetchCliente: async()=>{
         try {
-            const response = await axios.get('http://localhost:3001/cliente')
+            const response = await axios.get('http://localhost:3001/clientes')
             set({clientes: response.data})
         } catch (error) {
             console.log("Error fecthing clientes", error.message)
@@ -21,7 +21,7 @@ const useClienteStore = create((set)=>({
     },
     deleteCliente: async(ID_Cliente)=>{
         try {
-            const response = await axios.delete(`http://localhost:3001/cliente/${ID_Cliente}`)
+            const response = await axios.delete(`http://localhost:3001/clientes/${ID_Cliente}`)
             console.log("cliente delete:",response.data)
             set((state)=>({clientes: state.clientes.filter(cliente=>cliente.ID_Cliente !== ID_Cliente)})) // filtra todos lo estudiantes actualizados o
         } catch (error) {                                                               // diferentes del id eliminado
@@ -31,7 +31,7 @@ const useClienteStore = create((set)=>({
     //____----------Agregado---------------________
     updateCliente: async (ID_Cliente, updatedData) => {
         try {  // Realiza una solicitud PUT para actualizar el estudiante en el servidor.
-            const response = await axios.put(`http://localhost:3001/cliente/${ID_Cliente}`, updatedData)
+            const response = await axios.put(`http://localhost:3001/clientes/${ID_Cliente}`, updatedData)
             console.log("cliente updated:", response.data)
             // Actualiza el estado localmente, modificando solo el estudiante con el id coincidente.
             set((state) => ({clientes: state.clientes.map((cliente)=> cliente.ID_Cliente === ID_Cliente ? {...cliente, ...response.data} : cliente)})) // actualiza el estudiante en el estado
--- frontend/src/store/DetallePedidoStore.js
+++ frontend/src/store/DetallePedidoStore.js
@@ -3,17 +3,17 @@ import axios from 'axios'  // permite comunicar tu frontend con el backend
 
 const useDetallePedidoStore = create((set)=>({
     detallePedidos: [],
-    addDetallePedido: async(detalle)=>{
+    addDetallePedido: async(detallePedido)=>{
         try {
-            const response = await axios.post('http://localhost:3001/detalle',detalle)
+            const response = await axios.post('http://localhost:3001/detallePedido',detallePedido)
             set((state)=>({detallePedidos: [...state.detallePedidos, response.data]}))// crea una copia el "..."
         } catch (error) {
-            console.log("Error adding detalle", error.message)
+            console.log("Error adding detallePedido", error.message)
         }
     },
     fetchDetallePedido: async()=>{
         try {
-            const response = await axios.get('http://localhost:3001/detalle')
+            const response = await axios.get('http://localhost:3001/detallePedido')
             set({detallePedidos: response.data})
         } catch (error) {
             console.log("Error fecthing detallePedidos", error.message)
@@ -21,22 +21,22 @@ const useDetallePedidoStore = create((set)=>({
     },
     deleteDetallePedido: async(ID_Detalle)=>{
         try {
-            const response = await axios.delete(`http://localhost:3001/detalle/${ID_Detalle}`)
-            console.log("detalle delete:",response.data)
-            set((state)=>({detallePedidos: state.detallePedidos.filter(detalle=>detalle.ID_Detalle !== ID_Detalle)})) // filtra todos lo estudiantes actualizados o
+            const response = await axios.delete(`http://localhost:3001/detallePedido/${ID_Detalle}`)
+            console.log("detallePedido delete:",response.data)
+            set((state)=>({detallePedidos: state.detallePedidos.filter(detallePedido=>detallePedido.ID_Detalle !== ID_Detalle)})) // filtra todos lo estudiantes actualizados o
         } catch (error) {                                                               // diferentes del id eliminado
-            console.log("Error deleting detalle:", error.message)
+            console.log("Error deleting detallePedido:", error.message)
         }
     },
     //____----------Agregado---------------________
     updateDetallePedido: async (ID_Detalle, updatedData) => {
         try {  // Realiza una solicitud PUT para actualizar el estudiante en el servidor.
-            const response = await axios.put(`http://localhost:3001/detalle/${ID_Detalle}`, updatedData)
-            console.log("detalle updated:", response.data)
+            const response = await axios.put(`http://localhost:3001/detallePedido/${ID_Detalle}`, updatedData)
+            console.log("detallePedido updated:", response.data)
             // Actualiza el estado localmente, modificando solo el estudiante con el id coincidente.
-            set((state) => ({detallePedidos: state.detallePedidos.map((detalle)=> detalle.ID_Detalle === ID_Detalle ? {...detalle, ...response.data} : detalle)})) // actualiza el estudiante en el estado
+            set((state) => ({detallePedidos: state.detallePedidos.map((detallePedido)=> detallePedido.ID_Detalle === ID_Detalle ? {...detallePedido, ...response.data} : detallePedido)})) // actualiza el estudiante en el estado
         } catch (error) {
-            console.log("Error updating detalle:", error.message)
+            console.log("Error updating detallePedido:", error.message)
         }
     }
     
