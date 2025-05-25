c058e2f1b41fe8a9e201ee2ac0988ac3ec113272
ClonNetflix <ederangeloingabauitsta@gmail.com>
2025-05-25 14:40:42 -0500

    agregado su trabajo de antoni

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// src/App.js
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Principal from './components/Principal';
 import Administrador from './components/Administrador';
 import LoginForm from './components/LoginAdministrador';
-import ProtectedRoute from './components/Administrador/RutaSegura/RutaProtegida';
+import useAuthStore from './store/AuthStore';
 
 function App() {
+  const { isAuthenticated } = useAuthStore();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Principal />} />
-        <Route path='/loginFrom' element={<LoginForm />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path='/administrador' element={<Administrador />} />
-        </Route>
+        <Route path="/" element={<Principal />} />
+        <Route path="/loginFrom" element={<LoginForm />} />
+        
+        {/* Ruta protegida */}
+        <Route
+          path="/administrador"
+          element={
+            isAuthenticated ? (
+              <Administrador />
+            ) : (
+              <Navigate to="/loginFrom" replace />
+            )
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
